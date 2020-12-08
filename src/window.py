ed4278849b5f89930539c31052dc496c77e88edb
Herpiko Dwi Aguno <herpiko@gmail.com>
2020-12-08 18:07:52 +0000

    Add welcome screen before switching to a11y screen.

--- src/window.py
+++ src/window.py
@@ -16,9 +16,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import subprocess
+import time
 from gi.repository import Gtk
 from gi.repository import Gio
 from gi.repository import GLib
+import threading
 
 
 @Gtk.Template(resource_path='/org/blankon/blankonWelcome/window.ui')
@@ -26,8 +28,11 @@ class BlankonWelcomeWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'BlankonWelcomeWindow'
 
     SkipBackButton = Gtk.Template.Child()
+    NextButton = Gtk.Template.Child()
     Stacks = Gtk.Template.Child()
 
+    SpinnerBox = Gtk.Template.Child()
+    WelcomeBox = Gtk.Template.Child()
     MainBox = Gtk.Template.Child()
     SeeingBox = Gtk.Template.Child()
     HearingBox = Gtk.Template.Child()
@@ -41,11 +46,12 @@ class BlankonWelcomeWindow(Gtk.ApplicationWindow):
 
     SeeingMagnifierSwitch = Gtk.Template.Child()
 
-    isMainBox = True
+    currentView = "welcome"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        self.Stacks.set_visible_child(self.WelcomeBox)
+        self.NextButton.connect("clicked", self.a11y)
         self.SkipBackButton.connect("clicked", self.do_skip_back)
         self.SeeingButton.connect("clicked", self.show_seeing_box)
         self.HearingButton.connect("clicked", self.show_hearing_box)
@@ -53,35 +59,49 @@ class BlankonWelcomeWindow(Gtk.ApplicationWindow):
         self.PointingButton.connect("clicked", self.show_pointing_box)
         self.SeeingMagnifierSwitch.connect("state-set", self.enable_magnifier)
 
+
     def do_skip_back(self, button):
-        if (self.isMainBox):
-            self.close()
+        if self.currentView == "welcome" or self.currentView == "a11y":
+            self.NextButton.hide()
+            self.SkipBackButton.hide()
+            self.Stacks.set_visible_child(self.SpinnerBox)
+
+            # Use threading to avoid blocking UI
+            thread = threading.Thread(target=self.send_analytic)
+            thread.daemon = True
+            thread.start()
         else:
             self.Stacks.set_visible_child(self.MainBox)
             self.SkipBackButton.set_label("Finish")
-            self.isMainBox = True
+            self.currentView = "a11y"
+
+    def a11y(self, button):
+        self.NextButton.hide()
+        self.Stacks.set_visible_child(self.MainBox)
+        self.SkipBackButton.set_label("Finish")
+        self.currentView = "a11y"
 
     def show_seeing_box(self, button):
         self.SkipBackButton.show()
-        self.isMainBox = False
+        self.currentView = "seeing"
         self.SkipBackButton.set_label("Back")
         self.Stacks.set_visible_child(self.SeeingBox)
 
     def show_hearing_box(self, button):
         self.SkipBackButton.show()
-        self.isMainBox = False
+        self.currentView = "hearing"
         self.SkipBackButton.set_label("Back")
         self.Stacks.set_visible_child(self.HearingBox)
 
     def show_typing_box(self, button):
         self.SkipBackButton.show()
-        self.isMainBox = False
+        self.currentView = "typing"
         self.SkipBackButton.set_label("Back")
         self.Stacks.set_visible_child(self.TypingBox)
 
     def show_pointing_box(self, button):
         self.SkipBackButton.show()
-        self.isMainBox = False
+        self.currentView = "pointing"
         self.SkipBackButton.set_label("Back")
         self.Stacks.set_visible_child(self.PointingBox)
 
@@ -89,4 +109,10 @@ class BlankonWelcomeWindow(Gtk.ApplicationWindow):
         setting = Gio.Settings.new("org.gnome.desktop.a11y.applications")
         bool_value = GLib.Variant("b", state)
         setting.set_value("screen-magnifier-enabled", bool_value)
+
+    def send_analytic(self):
+        print("Send analytic data...")
+        time.sleep(1)
+        print("Data sent")
+        self.close()
       
